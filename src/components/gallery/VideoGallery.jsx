eb94a6e0bc22a89157c8e76cd2cdb77ce46b432e
T G <ghalumyan@icloud.com>
2024-12-29 18:16:00 +0400

    fix video

--- src/components/gallery/VideoGallery.jsx
+++ src/components/gallery/VideoGallery.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import videoSrc from "../../assets/Gallery.mp4";
 
 const VideoGallery = () => {
     const videoRef = useRef(null);
+    const [isMuted, setIsMuted] = useState(true);
 
     useEffect(() => {
         if (videoRef.current) {
-            videoRef.current.muted = true;
+            videoRef.current.muted = isMuted;
             videoRef.current.loop = true;
             videoRef.current.playsInline = true;
             videoRef.current.play();
         }
-    }, []);
+    }, [isMuted]);
+
+    const handleVideoClick = () => {
+        setIsMuted((prevMuted) => !prevMuted);
+    };
 
     return (
-        <div className="video-gallery__container">
+        <div className={`video-gallery__container ${isMuted ? "" : "video-gallery__container--sound-on"}`}>
             <video
                 ref={videoRef}
                 src={videoSrc}
                 className="video-gallery"
-                muted
+                muted={isMuted}
                 loop
                 playsInline
                 webkitPlaysinline
+                onClick={handleVideoClick}
             ></video>
         </div>
     );
